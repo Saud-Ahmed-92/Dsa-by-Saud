f9e6b55feb99f2421a6cbbf42ef5787d06769b29
Saud Ahmed <saudahmedkhan2005@gmail.com>
2025-06-17 23:31:41 +0500

    Add files via upload

---
+++ Circular Linked List.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stdlib.h> // Required for malloc
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* head = NULL; // Head of the circular linked list
+
+    cout << "Enter numbers to insert into the circular linked list (enter -1 to stop):\n";
+
+    while (true) {
+        Node* ptr = (Node*)malloc(sizeof(Node));
+        cin >> ptr->data;
+
+        if (ptr->data == -1) {
+            free(ptr);
+            break;
+        }
+
+        ptr->next = NULL;
+
+        if (head == NULL) {  // First node
+            head = ptr;
+            head->next = head;  // Circular link
+        } else {
+            Node* curr = head;
+            while (curr->next != head) { // Find last node
+                curr = curr->next;
+            }
+            curr->next = ptr;
+            ptr->next = head; // Maintain circular link
+        }
+    }
+
+    // Display the circular linked list
+    if (head != NULL) {
+        cout << "The circular linked list is: ";
+        Node* curr = head;
+        do {
+            cout << curr->data;
+            if (curr->next != head) cout << ", ";
+            curr = curr->next;
+        } while (curr != head);
+        cout << endl;
+    }
+
+    // Searching an element
+    int search;
+    cout << "Enter the number to search: ";
+    cin >> search;
+    Node* curr = head;
+    bool found = false;
+
+    if (head != NULL) {
+        do {
+            if (curr->data == search) {
+                found = true;
+                break;
+            }
+            curr = curr->next;
+        } while (curr != head);
+    }
+
+    if (found) cout << "Value Found\n";
+    else cout << "Not Found\n";
+
+}
